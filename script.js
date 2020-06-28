d56e64338498f48e159a70801819b37d8c59d53e
britneyio <britneyokhiria@icloud.com>
2020-06-28 13:46:35 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// const main = document.getElementById("main");
+// const blog_post = document.getElementsByClassName("blog-post");
+// const code = document.getElementsByTagName("code");
+
+// for(let i =0; i < code.length; i++){
+//     code[i].style.width = code[i].length;
+//     code[i].style.height = code[i].length*2;
+//     main.appendChild(code[i]);
+//     alert(code[i],  code[i].style.width,  code[i].style.height);
+// }
